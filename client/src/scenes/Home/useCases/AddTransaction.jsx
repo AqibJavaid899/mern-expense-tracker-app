3404e732507ffad2137c4fa64c4b43ef6b3955c5
AqibJavaid899 <aqibjavaid0910@gmail.com>
2022-12-13 22:27:52 +0500

    Add the Alert when the user submits the Transaction to the Database

--- client/src/scenes/Home/useCases/AddTransaction.jsx
+++ client/src/scenes/Home/useCases/AddTransaction.jsx
@@ -34,6 +34,7 @@ const AddTransaction = ({ createTransaction }) => {
     e.preventDefault();
     const result = await createTransaction(form);
     setForm(initialState);
+    alert("Transaction is added to the Database.");
   };
 
   return (
